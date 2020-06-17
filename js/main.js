9fe6c9c94567055143b8e6823925be0150c14a2c
parksuhyun9321 <59465364+parksuhyun9321@users.noreply.github.com>
2020-06-17 16:04:44 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
+$("#popup").slideDown(350);
+$()
+// 웹페이지 접속시 팝업창 구현
+
+$(".popupWrap .footer .btnClose").on("click",function(){
+    $(".popupWrap").fadeOut(250);
+});
+// 버튼 클릭시 팝업창 닫음
+
 
 $(".utilMenu").on("click",function(){
     $("#gnb").fadeIn(250);
-    $("body").addClass("overHidden");
     $(".utilMenu").hide();
     $("#logo").hide();
 
@@ -9,29 +17,29 @@ $(".utilMenu").on("click",function(){
 });
 $("#gnb .btnClose").on("click",function(){
     $("#gnb").fadeOut(250);
-    $("body").removeClass("overHidden");
     $(".utilMenu").show()
     $("#logo").show();
     return false;
 });
+
+// 버튼 클릭시 메뉴 
+
 let util = document.querySelector(".utilMenu");
 let logo = document.querySelector("#logo");
 let txt = document.querySelector(".txt");
 let gnb = document.querySelector("#gnb");
-window.addEventListener("scroll",function(){
+
+$(window).on("scroll",function(){
     let st = this.scrollY;
-    console.log("st", st)
-    if(st > 1200) {
+    // console.log("st", st)
+    if(st >= 1200) {
         gnb.classList.add("last");
+        txt.classList.add("last");
     } 
-    
     else {
         gnb.classList.remove("last");
-    } if (st > 1559) {
-        txt.classList.add("last");
-    } else {
         txt.classList.remove("last");
     }
-
 });
 
+//스크롤시 애니메이션
